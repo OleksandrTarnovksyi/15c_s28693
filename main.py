7138faa4de2319d3aa200b7abb17bf8bce61e506
Александр Тарновский <aleks.tarnovskyi@gmail.com>
2024-03-22 18:11:10 +0100

    Finished fourth task

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import math
+
+
 class SquareGenerator:
     def e_squares(self, start, end):
-        return [number * number for number in range(start, end)]
+        squares = [number * number for number in range(start, end)]
+        return [math.sqrt(square) for square in squares]
